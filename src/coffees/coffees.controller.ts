4b8beb9e0f79283d48ce2b4149a88e9ed696a407
Yucel Cekinmez <cekinmezyucel@gmail.com>
2023-10-27 09:40:21 +0200

    feat: add PATCH and DELETE methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
+  findAll(): string {
     return 'This action returns all coffees';
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): string {
     return `This action returns #${id} coffee`;
   }
 
   @Post()
   create(@Body() body: any) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any): string {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `This action removes #${id} coffee`;
   }
 }
