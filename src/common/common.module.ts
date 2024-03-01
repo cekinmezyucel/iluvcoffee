71fe9f84d7748c53f85f8b2b45f28dc71f48b9a3
Yucel Cekinmez <cekinmezyucel@gmail.com>
2024-03-01 15:36:13 +0100

    feat: add RequestTimeoutException interceptor for 3 seconds

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from './interceptors/wrap-response/wrap-response.interceptor';
+import { TimeoutInterceptor } from './interceptors/timeout/timeout.interceptor';
 
 @Module({
   imports: [ConfigModule],
   providers: [
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
     { provide: APP_GUARD, useClass: ApiKeyGuard },
+    { provide: APP_INTERCEPTOR, useClass: TimeoutInterceptor },
     { provide: APP_INTERCEPTOR, useClass: WrapResponseInterceptor },
   ],
 })
