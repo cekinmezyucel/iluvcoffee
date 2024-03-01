e9b6a712d3eb3f155f04502912a198acdb90c1f2
Yucel Cekinmez <cekinmezyucel@gmail.com>
2024-03-01 16:12:10 +0100

    feat: create a custom param decorator and send a parameter to the decorator implementation code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -14,13 +14,18 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Public } from '../common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int/parse-int.pipe';
+import { Protocol } from '../common/decorators/protocol.decorator';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
   @Get()
   @Public()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
+  findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
+    console.log(protocol);
     return this.coffeesService.findAll(paginationQuery);
   }
 
