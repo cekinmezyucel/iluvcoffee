3bc519adec93b61bcf28569479b78cc354e3e9f9
Yucel Cekinmez <cekinmezyucel@gmail.com>
2024-03-01 15:20:53 +0100

    feat: add interceptor for wrapping the response object into the data object

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
+import { WrapResponseInterceptor } from './interceptors/wrap-response/wrap-response.interceptor';
 
 @Module({
   imports: [ConfigModule],
   providers: [
-    {
-      provide: APP_GUARD,
-      useClass: ApiKeyGuard,
-    },
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    { provide: APP_GUARD, useClass: ApiKeyGuard },
+    { provide: APP_INTERCEPTOR, useClass: WrapResponseInterceptor },
   ],
 })
 export class CommonModule {}
