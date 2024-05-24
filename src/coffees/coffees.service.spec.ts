fe3d9736e011e967e7b11c9a753748865881c852
Yucel Cekinmez <cekinmezyucel@gmail.com>
2024-05-24 16:28:07 +0200

    feat: mock dependencies for CoffeesService
    prepare testing with jest

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { DataSource } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Flavor } from './entities/flavor.entity';
+import { Coffee } from './entities/coffee.entity';
+import coffeesConfig from './config/coffees.config';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: DataSource, useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+        { provide: 'COFFEE_BRANDS', useValue: {} },
+        { provide: coffeesConfig.KEY, useValue: { foo: 'bar' } },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
